4f20a8fd565c3298028a42d9d05a2d2d4928537f
GiovanaSoares1904 <giovanasoares_1804@outlook.com.br>
2026-06-23 20:17:08 -0300

    Atualização de CI.yml

--- main.py
+++ main.py
@@ -66,5 +66,5 @@ async def root():
         "restaurante": settings.app_name,
         "versao": settings.app_version,
         "limite_mesas": settings.max_mesas,
-        "status": "Feito a ateração. Olá do contêiner!" 
+        "status": "Feito nova ateração. Olá do contêiner!" 
     }
